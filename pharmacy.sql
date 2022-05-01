c48f12d2f4aa59e5a1736f59de1e2cc56c8a0939
Samuel Osei Adu <asamuel355@yahoo.com>
2022-05-01 12:34:11 +0000

    updates;

--- pharmacy.sql
+++ pharmacy.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Apr 30, 2022 at 11:21 PM
+-- Generation Time: May 01, 2022 at 02:23 PM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.1.5
 
@@ -70,6 +70,28 @@ INSERT INTO `customers` (`ID`, `NAME`, `CONTACT_NUMBER`, `ADDRESS`, `DOCTOR_NAME
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `diagnosis`
+--
+
+CREATE TABLE `diagnosis` (
+  `ID` int(11) NOT NULL,
+  `DIAGNOSE_NAME` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `diagnosis`
+--
+
+INSERT INTO `diagnosis` (`ID`, `DIAGNOSE_NAME`) VALUES
+(1, 'Headache'),
+(2, 'HIV/AIDS'),
+(3, 'Asthma'),
+(4, 'Stomach Ache'),
+(5, 'Malaria');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `invoices`
 --
 
@@ -87,9 +109,7 @@ CREATE TABLE `invoices` (
 --
 
 INSERT INTO `invoices` (`INVOICE_ID`, `NET_TOTAL`, `INVOICE_DATE`, `CUSTOMER_ID`, `TOTAL_AMOUNT`, `TOTAL_DISCOUNT`) VALUES
-(1, 30, '2021-10-19', 14, 30, 0),
-(2, 2626, '2021-10-19', 6, 2626, 0),
-(3, 32.65, '2022-04-30', 4, 32.65, 0);
+(8, 290, '2022-05-01', 6, 290, 0);
 
 -- --------------------------------------------------------
 
@@ -100,6 +120,7 @@ INSERT INTO `invoices` (`INVOICE_ID`, `NET_TOTAL`, `INVOICE_DATE`, `CUSTOMER_ID`
 CREATE TABLE `medicines` (
   `ID` int(11) NOT NULL,
   `NAME` varchar(100) COLLATE utf16_bin NOT NULL,
+  `DIAGNOSE_NAME` varchar(255) COLLATE utf16_bin DEFAULT NULL,
   `PACKING` varchar(20) COLLATE utf16_bin NOT NULL,
   `GENERIC_NAME` varchar(100) COLLATE utf16_bin NOT NULL,
   `SUPPLIER_NAME` varchar(100) COLLATE utf16_bin NOT NULL
@@ -109,11 +130,11 @@ CREATE TABLE `medicines` (
 -- Dumping data for table `medicines`
 --
 
-INSERT INTO `medicines` (`ID`, `NAME`, `PACKING`, `GENERIC_NAME`, `SUPPLIER_NAME`) VALUES
-(1, 'Nicip Plus', '10tab', 'Paracetamole', 'BDPL PHARMA'),
-(2, 'Crosin', '10tab', 'Hdsgvkvajkcbja', 'Kiran Pharma'),
-(4, 'Dolo 650', '15tab', 'paracetamole', 'BDPL PHARMA'),
-(5, 'Gelusil', '10tab', 'mint fla', 'Desai Pharma');
+INSERT INTO `medicines` (`ID`, `NAME`, `DIAGNOSE_NAME`, `PACKING`, `GENERIC_NAME`, `SUPPLIER_NAME`) VALUES
+(1, 'Nicip Plus', 'Headache', '10tab', 'Paracetamole', 'BDPL PHARMA'),
+(2, 'Crosin', 'Asthma', '10tab', 'Hdsgvkvajkcbja', 'Kiran Pharma'),
+(4, 'Dolo 650', 'HIV/AIDS', '15tab', 'paracetamole', 'BDPL PHARMA'),
+(5, 'Gelusil', 'Stomach Ache', '10tab', 'mint fla', 'Desai Pharma');
 
 -- --------------------------------------------------------
 
@@ -124,6 +145,7 @@ INSERT INTO `medicines` (`ID`, `NAME`, `PACKING`, `GENERIC_NAME`, `SUPPLIER_NAME
 CREATE TABLE `medicines_stock` (
   `ID` int(11) NOT NULL,
   `NAME` varchar(100) COLLATE utf16_bin NOT NULL,
+  `DIAGNOSE_NAME` varchar(255) COLLATE utf16_bin NOT NULL,
   `BATCH_ID` varchar(20) COLLATE utf16_bin NOT NULL,
   `EXPIRY_DATE` varchar(10) COLLATE utf16_bin NOT NULL,
   `QUANTITY` int(11) NOT NULL,
@@ -135,11 +157,11 @@ CREATE TABLE `medicines_stock` (
 -- Dumping data for table `medicines_stock`
 --
 
-INSERT INTO `medicines_stock` (`ID`, `NAME`, `BATCH_ID`, `EXPIRY_DATE`, `QUANTITY`, `MRP`, `RATE`) VALUES
-(1, 'Crosin', 'CROS12', '12/34', 2, 2626, 26),
-(2, 'Gelusil', 'G327', '12/42', 0, 15, 12),
-(3, 'Dolo 650', 'DOLO327', '01/23', 3, 30, 24),
-(4, 'Nicip Plus', 'NI325', '05/22', 2, 32.65, 28);
+INSERT INTO `medicines_stock` (`ID`, `NAME`, `DIAGNOSE_NAME`, `BATCH_ID`, `EXPIRY_DATE`, `QUANTITY`, `MRP`, `RATE`) VALUES
+(1, 'Crosin', 'Headache', 'CROS12', '12/34', 9, 260, 26),
+(2, 'Gelusil', 'Asthma', 'G327', '12/42', 8, 15, 12),
+(3, 'Dolo 650', 'HIV/AIDS', 'DOLO327', '01/23', 19, 30, 24),
+(4, 'Nicip Plus', 'Stomach Ache', 'NI325', '05/22', 10, 32, 28);
 
 -- --------------------------------------------------------
 
@@ -155,6 +177,33 @@ CREATE TABLE `purchases` (
   `TOTAL_AMOUNT` double NOT NULL,
   `PAYMENT_STATUS` varchar(20) COLLATE utf16_bin NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sales`
+--
+
+CREATE TABLE `sales` (
+  `ID` int(11) NOT NULL,
+  `CUSTOMER_ID` int(20) NOT NULL,
+  `INVOICE_NUMBER` int(25) NOT NULL,
+  `MEDICINE_NAME` varchar(255) NOT NULL,
+  `BATCH_ID` varchar(100) NOT NULL,
+  `EXPIRY_DATE` varchar(50) NOT NULL,
+  `QUANTITY` int(10) NOT NULL,
+  `MRP` int(10) NOT NULL,
+  `DISCOUNT` int(10) NOT NULL,
+  `TOTAL` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `sales`
+--
+
+INSERT INTO `sales` (`ID`, `CUSTOMER_ID`, `INVOICE_NUMBER`, `MEDICINE_NAME`, `BATCH_ID`, `EXPIRY_DATE`, `QUANTITY`, `MRP`, `DISCOUNT`, `TOTAL`) VALUES
+(1, 6, 8, 'Dolo 650', 'DOLO327', '01/23', 1, 30, 0, 30),
+(2, 6, 8, 'Crosin', 'CROS12', '12/34', 1, 260, 0, 260);
 
 -- --------------------------------------------------------
 
@@ -209,6 +258,12 @@ ALTER TABLE `customers`
   ADD PRIMARY KEY (`ID`);
 
 --
+-- Indexes for table `diagnosis`
+--
+ALTER TABLE `diagnosis`
+  ADD PRIMARY KEY (`ID`);
+
+--
 -- Indexes for table `invoices`
 --
 ALTER TABLE `invoices`
@@ -234,6 +289,12 @@ ALTER TABLE `purchases`
   ADD PRIMARY KEY (`VOUCHER_NUMBER`);
 
 --
+-- Indexes for table `sales`
+--
+ALTER TABLE `sales`
+  ADD PRIMARY KEY (`ID`);
+
+--
 -- Indexes for table `suppliers`
 --
 ALTER TABLE `suppliers`
@@ -256,10 +317,16 @@ ALTER TABLE `customers`
   MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
+-- AUTO_INCREMENT for table `diagnosis`
+--
+ALTER TABLE `diagnosis`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
 -- AUTO_INCREMENT for table `invoices`
 --
 ALTER TABLE `invoices`
-  MODIFY `INVOICE_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `INVOICE_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `medicines`
@@ -278,6 +345,12 @@ ALTER TABLE `medicines_stock`
 --
 ALTER TABLE `purchases`
   MODIFY `VOUCHER_NUMBER` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `sales`
+--
+ALTER TABLE `sales`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `suppliers`
