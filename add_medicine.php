1746c51cf04547e1dfd5f0b59aeae31f64507840
Samuel Osei Adu <asamuel355@yahoo.com>
2022-05-05 11:41:18 +0000

    updates

--- add_medicine.php
+++ add_medicine.php
@@ -1,37 +1,21 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-    <head>
-      <meta charset="utf-8">
-      <title>Add New Medicine</title>
-      <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-      <script src="bootstrap/js/jquery.min.js"></script>
-      <script src="bootstrap/js/bootstrap.min.js"></script>
-      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
-      <link rel="shortcut icon" href="images/icon.svg" type="image/x-icon">
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-      <link rel="stylesheet" href="css/sidenav.css">
-      <link rel="stylesheet" href="css/home.css">
-      <script src="js/suggestions.js"></script>
-      <script src="js/validateForm.js"></script>
-      <script src="js/restrict.js"></script>
-    </head>
+  <head>
+    <meta charset="utf-8">
+    <title>Add New Purchase</title>
+    <script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+		<script src="bootstrap/js/jquery.min.js"></script>
+		<script src="bootstrap/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+    <link rel="shortcut icon" href="images/icon.svg" type="image/x-icon">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/sidenav.css">
+    <link rel="stylesheet" href="css/home.css">
+    <script src="js/restrict.js"></script>
+  </head>
   <body>
-    <div id="add_new_supplier_model">
-      <div class="modal-dialog">
-      	<div class="modal-content">
-      		<div class="modal-header" style="background-color: #ff5252; color: white">
-            <div class="font-weight-bold">Add New Supplier</div>
-      			<button class="close" style="outline: none;" onclick="document.getElementById('add_new_supplier_model').style.display = 'none';"><i class="fa fa-close"></i></button>
-      		</div>
-      		<div class="modal-body">
-            <?php
-              include('sections/add_new_supplier.html');
-            ?>
-      		</div>
-      	</div>
-      </div>
-    </div>
-    <!-- including side navigations -->
+    <!-- including side navigation -->
     <?php include("sections/sidenav.html"); ?>
 
     <div class="container-fluid">
@@ -40,76 +24,236 @@
         <!-- header section -->
         <?php
           require "php/header.php";
-          createHeader('shopping-bag', 'Add Medicine', 'Add New Medicine');
+          createHeader('bar-chart', 'Add Inventory', 'Add New Drugs');
         ?>
         <!-- header section end -->
 
         <!-- form content -->
         <div class="row">
-          <div class="row col col-md-6">
-            <div class="row col col-md-12">
-              <div class="col col-md-8 form-group">
-                <label class="font-weight-bold" for="medicine_name">Medicine Name :</label>
-                <input type="text" class="form-control" id="medicine_name" placeholder="Medicine Name" onblur="notNull(this.value, 'medicine_name_error');">
-                <code class="text-danger small font-weight-bold float-right" id="medicine_name_error" style="display: none;"></code>
-              </div>
-              <div class="col col-md-4 form-group">
-                <label class="font-weight-bold" for="packing">Packing :</label>
-                <input type="text" class="form-control" id="packing" placeholder="Packing e.g. 10 TAB / 100 ML" onblur="notNull(this.value, 'pack_error');">
-                <code class="text-danger small font-weight-bold float-right" id="pack_error" style="display: none;"></code>
-              </div>
-          </div>
-
+          <!-- manufacturer details content -->
           <div class="row col col-md-12">
-            <div class="col col-md-12 form-group">
-              <label class="font-weight-bold" for="generic_name">Generic Name :</label>
-              <input type="text" class="form-control" id="generic_name" placeholder="Generic Name" onblur="notNull(this.value, 'generic_name_error');">
-              <code class="text-danger small font-weight-bold float-right" id="generic_name_error" style="display: none;"></code>
+            <div class="container">
+              <form id="add-drugs" method="POST" class="form">
+                <div class="row">
+                  <div style="display: none;" class="col-12 text-center alert alert-danger error-text"></div>
+                </div>
+                <div class="row">
+                  <div class="col-md-4 col-12">
+                    <div class="form-group">
+                      <label for="drug-name"> Drug Name</label>
+                      <input type="text" name="drug-name" id="drug-name" class="form-control">
+                      <span class="text-danger drug-name-error mt-3"></span>
+                    </div>
+                  </div>
+                  <div class="col-md-4 col-12">
+                    <div class="form-group">
+                      <label for="diagnose-name">Diagnose Name</label>
+                      <select name="diagnose-name" id="diagnose-name" class="form-group custom-select">
+                        <option value="select">Select Diagnose Name</option>
+                        <?php
+                          include_once "php/db_connection.php";
+                          $query = "SELECT * FROM diagnosis";
+                          $qry_all = mysqli_query($con, $query);
+                          while ($row = mysqli_fetch_array($qry_all)){
+                            $diagnose_id = $row['ID'];
+                            $diagnose_name = $row['DIAGNOSE_NAME'];
+                            echo '
+                              <option value="'.$diagnose_name.'">'.$diagnose_name.'</option>
+                            ';
+                          }
+                        ?>
+                        
+                      </select>
+                      <span class="text-danger diagnose-name-error"></span>
+                    </div>
+                  </div>
+                  <div class="col-md-4 col-12">
+                    <div class="form-group">
+                      <label for="generic-name"> Generic Name</label>
+                      <input type="text" name="generic-name" id="generic-name" class="form-control">
+                      <span class="text-danger generic-name-error"></span>
+                    </div>
+                  </div>
+                  <div class="col-md-4 col-12">
+                    <div class="form-group">
+                      <label for="expiry-date"> Expiry Date (mm/yr)</label>
+                      <input type="text" name="expiry-date" id="expiry-date" class="form-control" placeholder="02/22">
+                      <span class="text-danger expiry-date-error"></span>
+                    </div>
+                  </div>
+                  <div class="col-md-4 col-12">
+                    <div class="form-group">
+                      <label for="quantity"> Quantity</label>
+                      <input type="number" name="quantity" id="quantity" class="form-control">
+                      <span class="text-danger quantity-error"></span>
+                    </div>
+                  </div>
+                  <div class="col-md-4 col-12">
+                    <div class="form-group">
+                      <label for="price"> Price</label>
+                      <input type="number" name="price" id="price" class="form-control">
+                      <span class="text-danger price-error"></span>
+                    </div>
+                  </div>
+                  <div class="col-md-4 col-12">
+                    <div class="form-group">
+                      <label for="diagnose-name">Supplier Name</label>
+                      <select name="supplier-name" id="supplier-name" class="form-group custom-select">
+                        <option value="select">Select Supplier Name</option>
+                        <?php
+                          include_once "php/db_connection.php";
+                          $query = "SELECT * FROM suppliers";
+                          $qry_all = mysqli_query($con, $query);
+                          while ($row = mysqli_fetch_array($qry_all)){
+                            $supplier_id = $row['ID'];
+                            $supplier_name = $row['NAME'];
+                            echo '
+                              <option value="'.$supplier_id.'">'.$supplier_name.'</option>
+                            ';
+                          }
+                        ?>
+                        
+                      </select>
+                      <span class="text-danger supplier-name-error"></span>
+                    </div>
+                  </div>
+                  <div class="col-md-8 col-12">
+                    <div class="form-group supplier-details" style="display: flex; justify-content: space-between">
+                        
+                    </div>
+                  </div>
+                  <div class="mt-4 col-md-12 col-12 text-center">
+                    <button class="btn btn-primary p-2">Add Drug</button>
+                  </div>
+                </div>
+              </form>
             </div>
           </div>
+          <!-- closing button -->
+          <div id="purchase_acknowledgement" class="col-md-12 h5 text-success font-weight-bold text-center" style="font-family: sans-serif;"></div>
 
-          <div class="row col col-md-12">
-            <div class="col col-md-12 form-group">
-              <label class="font-weight-bold" for="suppliers_name">Supplier :</label>
-              <input id="suppliers_name" type="text" class="form-control" id="suppliers_name" placeholder="Supplier Name" name="suppliers_name" onkeyup="showSuggestions(this.value, 'supplier');">
-              <code class="text-danger small font-weight-bold float-right" id="supplier_name_error" style="display: none;"></code>
-              <div id="supplier_suggestions" class="list-group position-fixed" style="z-index: 1; width: 35.80%; overflow: auto; max-height: 200px;"></div>
-            </div>
-          </div>
-
-          <div class="row col col-md-12">
-              <div class="col col-md-5 font-weight-bold" style="color: green;cursor:pointer" onclick="document.getElementById('add_new_supplier_model').style.display = 'block';">
-                <i class="fa fa-plus"></i>Add New Supplier
-              </div>
-          </div>
-<hr>
-
-<div class="col col-md-12">
-  <hr class="col-md-12 float-left" style="padding: 0px; width: 95%; border-top: 2px solid  #02b6ff;">
-</div>
-
-<!-- new user button -->
-<div class="row col col-md-12">
-  &emsp;
-  <div class="form-group m-auto">
-    <button class="btn btn-primary form-control" onclick="addMedicine();">ADD</button>
-  </div>
-  <!--
-  &emsp;
-  <div class="form-group">
-    <button class="btn btn-success form-control">Save and Add Another</button>
-  </div>
--->
-</div>
-<!-- customer details content end -->
-<!-- result message -->
-<div id="medicine_acknowledgement" class="col-md-12 h5 text-success font-weight-bold text-center" style="font-family: sans-serif;"></div>
-          </div>
         </div>
-
-        <hr style="border-top: 2px solid #ff5252;">
         <!-- form content end -->
+        <hr style="border-top: 2px solid #ff5252;">
       </div>
     </div>
   </body>
+
+  <script>
+    $(document).ready(function(){
+      $('#supplier-name').on('change', function(){
+        if($.trim($('#supplier-name').val()) == 'select'){
+          alert('Select supplier');
+          $('.supplier-details').css('display', 'none');
+          return false;
+        }else{
+          var id = $(this).val();
+          $.ajax({
+            url: 'php/getSupplier.php',
+            method: 'GET',
+            data: {id: id},
+
+            success: function(data){
+              $('.supplier-details').css('display', 'block');
+              $('.supplier-details').html(data);
+            }
+          })
+        }
+      });
+
+      //Add Drugs
+      $('#add-drugs').on('submit', function(e){
+        e.preventDefault();
+      
+        if($.trim($('#drug-name').val()).length == 0 ){
+          var errorMsg = 'Enter Drug Name';
+          $('.drug-name-error').html(errorMsg)
+        }else{
+          errorMsg = '';
+          $('.drug-name-error').html(errorMsg)
+        }
+
+        if($.trim($('#diagnose-name').val()) == 'select' ){
+          var errorMsg = 'Select Diagnose Name';
+          $('.diagnose-name-error').html(errorMsg)
+        }else{
+          errorMsg = '';
+          $('.diagnose-name-error').html(errorMsg)
+        }
+
+        if($.trim($('#generic-name').val()).length == 0 ){
+          var errorMsg = 'Enter Generic Name';
+          $('.generic-name-error').html(errorMsg)
+        }else{
+          errorMsg = '';
+          $('.generic-name-error').html(errorMsg)
+        }
+
+        if($.trim($('#expiry-date').val()).length == 0 ){
+          var errorMsg = 'Check date format and add';
+          $('.expiry-date-error').html(errorMsg)
+        }else{
+          if($.trim($('#expiry-date').val()).length > 8 ){
+            var errorMsg = 'Check date format and add';
+            $('.expiry-date-error').html(errorMsg)
+          }else{
+            errorMsg = '';
+            $('.expiry-date-error').html(errorMsg)
+          }
+        }
+
+        if($.trim($('#quantity').val()).length == 0 ){
+          var errorMsg = 'Enter drugs quantity';
+          $('.quantity-error').html(errorMsg)
+        }else{
+          errorMsg = '';
+          $('.quantity-error').html(errorMsg)
+        }
+
+        if($.trim($('#price').val()).length == 0 ){
+          var errorMsg = 'Enter Drug Unit Price';
+          $('.price-error').html(errorMsg)
+        }else{
+          errorMsg = '';
+          $('.price-error').html(errorMsg)
+        }
+
+        if($.trim($('#supplier-name').val()) == 'select' ){
+          var errorMsg = 'Select Supplier Name';
+          $('.supplier-name-error').html(errorMsg)
+        }else{
+          errorMsg = '';
+          $('.supplier-name-error').html(errorMsg)
+        }
+
+        if($.trim($('#drug-name').val()).length == 0 || $.trim($('#diagnose-name').val()) == 'select' || $.trim($('#generic-name').val()).length == 0 || $.trim($('#expiry-date').val()).length == 0 || $.trim($('#quantity').val()).length == 0 || $.trim($('#price').val()).length == 0 || $.trim($('#supplier-name').val()) == 'select' ){
+          var errMsg = 'Check all fields and fill correctly';
+          $('.error-text').css('display', 'block'),
+          $('.error-text').html(errMsg).fadeOut(8000);
+
+        }else{
+          $.ajax({
+            url: 'php/add-new-drug.php',
+            method: 'POST',
+            data: $('#add-drugs').serialize(),
+            success: function(data){
+              if(data === 'success'){
+                alert('Added successfully')
+                $('#add-drugs')[0].reset()
+                $('.supplier-details').css('display', 'none');
+                window.location.reload(true)
+              }else{
+                $('.error-text').css('display', 'block'),
+                $('.error-text').html(data).fadeOut(8000);
+              }
+            },
+            error: function(err){
+              console.log(err)
+            }
+          })
+          
+        }
+      })
+    })
+  </script>
 </html>
