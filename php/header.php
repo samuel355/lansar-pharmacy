aa56554851ff2c95dfcf519c6f7b5ffd6bd054d8
Samuel Osei Adu <asamuel355@yahoo.com>
2022-05-05 22:06:37 +0000

    new updates

--- php/header.php
+++ php/header.php
@@ -7,9 +7,6 @@
         &emsp;&emsp;&emsp;<small class="font-weight-bold h6">'.$sub_heading.'</small>
       </div>
       <div class="col-md-1 user_options">
-        <button class="col col-md-1 m-3" onclick="showOptions();">
-          <i class="fa fa-gear"></i>
-        </button>
         <div id="mark"><i class="fa fa-play fa-rotate-270"></i></div>
         <ul id="options" class="options list-unstyled" style="display: none;">
           <li>
